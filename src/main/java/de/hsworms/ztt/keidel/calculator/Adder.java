440ae0716e642fe3cc2aeb080da8f4a6f8f85078
Ralf Keidel <keidel@hs-worms.de>
2018-10-11 13:51:51 +0200

    code: implement adder with test

---
+++ src/main/java/de/hsworms/ztt/keidel/calculator/Adder.java
@@ -0,0 +1,8 @@
+package de.hsworms.ztt.keidel.calculator;
+
+public class Adder {
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/de/hsworms/ztt/keidel/calculator/AdderTest.java
@@ -0,0 +1,20 @@
+package de.hsworms.ztt.keidel.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdderTest {
+
+    private static double DELTA = 0.0000001;
+
+    @Test
+    public void add() {
+        assertEquals(2., Adder.add(1., 1.), DELTA );
+
+        assertEquals(Double.MAX_VALUE, Adder.add(0., Double.MAX_VALUE), DELTA );
+
+        assertEquals(2. * Double.MAX_VALUE, Adder.add(Double.MAX_VALUE, Double.MAX_VALUE), DELTA );
+        assertTrue(Adder.add(Double.MAX_VALUE, Double.MAX_VALUE) > 0.);
+    }
+}

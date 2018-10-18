2cb7155ed0ccd07f371f97d0ac4e5adadd7e469a
Ralf Keidel <keidel@hs-worms.de>
2018-10-18 11:50:21 +0200

    test: implement functional test for Stack Exception (not ready yet)

--- src/main/java/de/hsworms/ztt/keidel/calculator/Main.java
+++ src/main/java/de/hsworms/ztt/keidel/calculator/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        StackInterface stack = new StackImpl();
+        try {
+            stack.pop();
+        } catch (IndexOutOfBoundsException e) {
+            // what to do now?
+        }
     }
 }

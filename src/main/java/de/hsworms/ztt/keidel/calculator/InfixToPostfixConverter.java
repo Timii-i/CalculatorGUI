434cc980ecc02903011187dbdc256ea591c36a5b
Ralf Keidel <keidel@hs-worms.de>
2018-12-13 10:51:16 +0100

    code: implement infix to postfix conversion (test first approach)

---
+++ src/main/java/de/hsworms/ztt/keidel/calculator/InfixToPostfixConverter.java
@@ -0,0 +1,12 @@
+package de.hsworms.ztt.keidel.calculator;
+
+public class InfixToPostfixConverter {
+
+
+    public static String toPostfix(String infix) {
+        // FIXME implement
+        return null;
+    }
+
+
+}

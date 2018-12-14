7957e10b1d8a17ca5c2eaf7d5af14f9b264fd3a3
Ralf Keidel <keidel@hs-worms.de>
2018-12-14 22:27:29 +0100

    code: Reverse Polish Notation based calculator with 'left to right algorithm'

---
+++ src/main/java/de/hsworms/ztt/keidel/calculator/Calculator.java
@@ -0,0 +1,51 @@
+package de.hsworms.ztt.keidel.calculator;
+
+import de.hsworms.ztt.keidel.calculator.tokenizer.Token;
+import de.hsworms.ztt.keidel.calculator.tokenizer.TokenizerUtil;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * For an idea how it works:
+ * <a href="https://en.wikipedia.org/wiki/Reverse_Polish_notation">
+ * Wikipedia: Reverse Polish notation (left-to-right algorithm)</a>
+ */
+public class Calculator {
+
+    public static int getResult(String infix) throws IOException {
+        Stack<Token> tokenStack = new Stack<>();
+        List<Token> tokenList = InfixToPostfixConverter.toPostfixListOfToken(infix);
+        for (Token token : tokenList) {
+            switch (token.getType()) {
+                case LITERAL:
+                    tokenStack.push(token);
+                    break;
+                case OPERATOR:
+                    int operandB = Integer.valueOf(tokenStack.pop().getValue());
+                    int operandA = Integer.valueOf(tokenStack.pop().getValue());
+                    switch (token.getOperator()) {
+                        case ADD:
+                            tokenStack.push(new Token(String.valueOf(operandA + operandB)));
+                            break;
+                        case SUBTRACT:
+                            tokenStack.push(new Token(String.valueOf(operandA - operandB)));
+                            break;
+                        case MULTIPLY:
+                            tokenStack.push(new Token(String.valueOf(operandA * operandB)));
+                            break;
+                        case DIVIDE:
+                            tokenStack.push(new Token(String.valueOf(operandA / operandB)));
+                            break;
+                        default:
+                            throw new IllegalStateException("Programing Error! Implement: " + token.getOperator());
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Programing Error! Implement: " + token.getType());
+            }
+        }
+        return Integer.valueOf(tokenStack.pop().getValue());
+    }
+}
---
+++ src/test/java/de/hsworms/ztt/keidel/calculator/CalculatorTest.java
@@ -0,0 +1,25 @@
+package de.hsworms.ztt.keidel.calculator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void getResult() throws IOException {
+        assertEquals(2, Calculator.getResult("(1+1)"));
+        assertEquals(4, Calculator.getResult("(1+1)*2"));
+        assertEquals(2, Calculator.getResult("(1+1)*(2 - 1)"));
+        assertEquals(121, Calculator.getResult("(120+1)*(2 - 1)"));
+        assertEquals(121, Calculator.getResult("(120+1)/(2 - 1)"));
+        assertEquals(12, Calculator.getResult("((120+1) - 1)/(11 - 1)"));
+        assertEquals(0, Calculator.getResult("(   1 -1)"));
+        assertEquals(-1, Calculator.getResult("(   1 -2)"));
+
+    }
+}

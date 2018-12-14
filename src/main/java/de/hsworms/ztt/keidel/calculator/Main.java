4e2e28c03f7206358ecc038cc080d59c98364756
Ralf Keidel <keidel@hs-worms.de>
2018-12-14 22:29:10 +0100

    code: implement console interface

--- src/main/java/de/hsworms/ztt/keidel/calculator/Main.java
+++ src/main/java/de/hsworms/ztt/keidel/calculator/Main.java
@@ -1,14 +1,22 @@
 package de.hsworms.ztt.keidel.calculator;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        StackInterface stack = new StackImpl();
-        try {
-            stack.pop();
-        } catch (IndexOutOfBoundsException e) {
-            // what to do now?
+
+        while(true) {
+            System.out.println("Please type an infix mathematical expression:");
+            Scanner scanner = new Scanner(System.in);
+            String infix = scanner.nextLine();
+
+            try {
+                System.out.println(infix + " = " + Calculator.getResult(infix));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }

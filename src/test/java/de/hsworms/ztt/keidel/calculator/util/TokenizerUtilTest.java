cbe9fe4763bb7e160212f92196594c34e3347787
Ralf Keidel <keidel@hs-worms.de>
2018-12-14 09:55:57 +0100

    code: Tokenizer accepting white space and digital numbers bigger than nine

---
+++ src/test/java/de/hsworms/ztt/keidel/calculator/util/TokenizerUtilTest.java
@@ -0,0 +1,25 @@
+package de.hsworms.ztt.keidel.calculator.util;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class TokenizerUtilTest {
+
+    @Test
+    public void tokenize() throws IOException {
+
+            assertEquals(
+                    "[4.0, *, x, +, 5.2024, *, (, Log, (, x, ,, y, ), ^, z, ), -, 300.12]",
+                    TokenizerUtil.tokenize("4*x+5.2024*(Log(x,y)^z)-300.12").toString());
+            assertEquals(
+                    "[4.0, *, x, +, 5.2024, *, (, Log, (, x, ,, y, ), ^, z, ), -, 300.12]",
+                    TokenizerUtil.tokenize("4 * x + 5.2024 * (Log(x,y)^z) - 300.12").toString());
+            assertEquals(
+                    "[4.0, *, x, +, 5.2024, *, (, Log, (, x, ,, y, ), ^, z, ), -, 300.12]",
+                    TokenizerUtil.tokenize("4   * x +   5.2024 *         " +
+                            "(Log(x,y)^z) - 300.12").toString());
+    }
+}

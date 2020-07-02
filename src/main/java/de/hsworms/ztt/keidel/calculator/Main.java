aa76c8abf6b5bcbd6ab2e81519ac76c88d1fb858
Tim Thomas <inf3253@hs-worms.de>
2020-07-02 17:25:11 +0200

    code: created basic window with one button in it

--- src/main/java/de/hsworms/ztt/keidel/calculator/Main.java
+++ src/main/java/de/hsworms/ztt/keidel/calculator/Main.java
@@ -3,11 +3,21 @@ package de.hsworms.ztt.keidel.calculator;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+// imports for javafx
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
 
     public static void main(String[] args) {
+        launch(args);
 
-        while(true) {
+        /*while(true) {
             System.out.println("Please type an infix mathematical expression:");
             Scanner scanner = new Scanner(System.in);
             String infix = scanner.nextLine();
@@ -17,7 +27,31 @@ public class Main {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
+    }
+
+    /**
+     * Start a given stage
+     *
+     * @param stage     the stage that should be started
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Sets the title of the stage
+        stage.setTitle("Title of the Window");
+
+        // Initiate a new instance of a button
+        button = new Button();
+        button.setText("Click me");
+
+        // Create a new layout and add the button to it
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout,300, 250);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static double getCalculatorResult(String infix) throws IOException {
